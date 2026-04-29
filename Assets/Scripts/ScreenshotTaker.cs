a8456ceeb2754c67511b7af5bc564e79880c8472
Bonaventhure Turpyn <109552357+DeBanaanMeneer@users.noreply.github.com>
2026-04-29 19:11:18 +0200

    i

---
+++ Assets/Scripts/ScreenshotTaker.cs
@@ -0,0 +1,34 @@
+//by Dante Deketele
+//DAE 2023-2024
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ScreenshotTaker : MonoBehaviour
+{
+    private void Update()
+    {
+        // New Input System syntax
+        if (Keyboard.current != null && Keyboard.current.sKey.wasPressedThisFrame)
+        {
+            TakeScreenshot();
+        }
+    }
+
+    static void TakeScreenshot(int index = 0)
+    {
+        if (Application.isPlaying)
+        {
+            //Make sure you have the Screenshots folder set up, the script won't generate it/
+			string path = "Assets/Screenshots";
+            var time = System.DateTime.Now;
+            string timestamp = $"{time.Hour}{time.Minute}{time.Second}";
+            string fileName = $"{path}/screenshot_{index}-{timestamp}.png";
+
+            ScreenCapture.CaptureScreenshot(fileName);
+
+            Debug.Log("Took screenshot: " + fileName);
+        }
+    }
+}
